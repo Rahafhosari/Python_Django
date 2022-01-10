9e21b0588b081ffb06cf4a95b004518a0292a861
Hadeel Yassin <hadeel.yasin@axsos.me>
2022-01-10 10:48:39 +0200

    trying conflict

--- GenericApiView.py
+++ GenericApiView.py
@@ -10,13 +10,13 @@ from rest_framework import mixins, views
 from rest_framework.settings import api_settings
 
 
-def get_object_or_404(queryset, *filter_args, **filter_kwargs):
+def get_object_or_404(queryset):
     """
     Same as Django's standard shortcut, but make sure to also raise 404
     if the filter_kwargs don't match the required types.
     """
     try:
-        return _get_object_or_404(queryset, *filter_args, **filter_kwargs)
+        return _get_object_or_404(queryset)
     except (TypeError, ValueError, ValidationError):
         raise Http404
 
